51e37426e3d4d36468d6e35dace6c0692550e2c2
Christian Bundy <christianbundy@fraction.io>
2019-09-18 13:25:56 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const flume = require('flumedb')
+const log = require('flumelog-offset')
+const os = require('os')
+const path = require('path')
+const pull = require('pull-stream')
+const mv = require('mv')
+
+const yargs = require('yargs')
+
+const config = yargs
+  .usage('Usage: $0 [options]')
+  .options('path', {
+    describe: 'database path',
+    default: path.join(os.homedir(), '.ssb', 'flume', 'log.offset'),
+    type: 'string'
+  })
+  .argv
+
+const createDb = (file) =>
+  flume(log(file, {
+    codec: {
+      encode: JSON.stringify,
+      decode: (data) => {
+        if (data.length >= 0x3fffffe7) {
+          // way too long
+          return null
+        }
+
+        try {
+          return JSON.parse(data)
+        } catch (e) {
+          console.log(`Ignored error: ${e}`)
+          return null
+        }
+      }
+    }
+  }))
+
+const paths = {
+  db: config.path,
+  tmp: '/tmp/db'
+}
+
+const a = createDb(paths.db)
+const b = createDb(paths.tmp)
+
+const onEachMessage = msg => {
+  b.append(msg, function (err) {
+    if (err) throw err
+  })
+}
+
+const onDone = () => {
+  // overwrite the real db with the temporary db
+  mv(paths.tmp, paths.db, function (err) {
+    if (err) throw err
+  })
+}
+
+pull(
+  a.stream({ seqs: false }),
+  pull.filter(),
+  pull.drain(onEachMessage, onDone)
+)
